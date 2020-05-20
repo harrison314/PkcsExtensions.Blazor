a1f181055a690d33993f0f6c7b2659c5318a5629
harrison314 <harrison314@azet.sk>
2020-05-20 20:33:05 +0200

    Move files from repozitory PkcsExtensions.

---
+++ src/src/PkcsExtenions.Blazor/ASN1/IAsn1Node.cs
@@ -0,0 +1,14 @@
+﻿using PkcsExtenions.ASN1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PkcsExtenions.Blazor.ASN1
+{
+    internal interface IAsn1Node
+    {
+        void Write(AsnWriter asnWriter);
+    }
+}
